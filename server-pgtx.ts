f88a3028bcc7579cc668e6fda0d31d73c061dfb3
M2K-5F <ivan.semjonow15@gmail.com>
2026-08-14 10:11:10 +0300

    fix

--- server-pgtx.ts
+++ server-pgtx.ts
@@ -7,7 +7,7 @@ const pool = new Pool({
   user: process.env.PGUSER || 'postgres',
   password: process.env.PGPASSWORD || 'postgres',
   database: process.env.PGDATABASE || 'pgtx_test',
-  max: Number(process.env.PGMAX) || 10 
+  max: Number(process.env.PGMAX) || 1
 });
 
 interface User {
