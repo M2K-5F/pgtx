78a463fbd4eb9b6fab3691a3d25b6557ca28b749
M2K-5F <ivan.semjonow15@gmail.com>
2026-08-19 19:06:02 +0300

    update tests

--- server-pgtx.ts
+++ server-pgtx.ts
@@ -44,9 +44,7 @@ const server = createServer(async (req, res) => {
     }
 
     for (const user of users) {
-      if (user.id !== targetIds[counter++]) {
-        console.log(users, targetIds);
-        
+      if (user.id !== targetIds[counter++]) {        
         res.writeHead(500, { 'Content-Type': 'application/json' })
         res.end(JSON.stringify({ error: 'Id mismatch' }))
         return
@@ -55,6 +53,58 @@ const server = createServer(async (req, res) => {
 
     res.writeHead(200, { 'Content-Type': 'application/json' })
     res.end(JSON.stringify(users))
+    return
+  }
+
+  if (req.url === '/user') {
+    const targetId = Math.floor(Math.random() * 1000) + 1
+
+    const user = await pool.query<User>`
+      SELECT id, name, balance FROM bench_users WHERE id = ${targetId}
+    `
+    .orElse(err => {
+      console.log(err)      
+      return Ok(null)
+    })
+
+    if (!user) {
+      res.writeHead(504, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify({ error: 'Timeout' }))
+      return
+    }
+
+    if (user[0].id !== targetId) {
+      res.writeHead(500, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify({ error: 'ID mismatch' }))
+      return
+    }
+
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(user))
+    return
+  }
+
+  if (req.url === '/simple') {
+    const [row] = await pool.query<{res: 1}>`SELECT 1 as res`
+    .orElse(err => {
+      console.log(err)
+      return Ok([])
+    })
+
+    if (!row) {
+      res.writeHead(504, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify({ error: 'Timeout' }))
+      return
+    }
+
+    if (row.res !== 1) {
+      res.writeHead(500, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify({ error: 'Res mismatch' }))
+      return
+    }
+
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(row))
     return
   }
 
--- server-postgresjs.ts
+++ server-postgresjs.ts
@@ -23,7 +23,6 @@ const server = createServer(async (req, res) => {
       order by id
     `.catch(err => {
       console.log(err)
-      
       return null
     })
   
@@ -48,6 +47,58 @@ const server = createServer(async (req, res) => {
     res.end(JSON.stringify(users))
     return
   }
+
+  if (req.url === '/user') {
+      const targetId = Math.floor(Math.random() * 1000) + 1
+  
+      const user = await sql`
+        SELECT id, name, balance FROM bench_users WHERE id = ${targetId}
+      `
+      .catch(err => {
+        console.log(err)      
+        return null
+      })
+  
+      if (!user) {
+        res.writeHead(504, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({ error: 'Timeout' }))
+        return
+      }
+  
+      if (user[0].id !== targetId) {
+        res.writeHead(500, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({ error: 'ID mismatch' }))
+        return
+      }
+  
+      res.writeHead(200, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify(user))
+      return
+    }
+  
+    if (req.url === '/simple') {
+      const [row] = await sql<{res: 1}[]>`SELECT 1 as res`
+      .catch(err => {
+        console.log(err)
+        return []
+      })
+  
+      if (!row) {
+        res.writeHead(504, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({ error: 'Timeout' }))
+        return
+      }
+  
+      if (row.res !== 1) {
+        res.writeHead(500, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({ error: 'Res mismatch' }))
+        return
+      }
+  
+      res.writeHead(200, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify(row))
+      return
+    }
 
   res.writeHead(404)
   res.end()
